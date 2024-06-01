43db156c7c8030bbdf60687f604b104b7e6c5fd2
sameerhussain3211 <sameerhussain44332211@gmail.com>
2024-06-02 00:30:05 +0500

    third

---
+++ happy_shop/lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:happy_shop/app_view.dart';
+import 'package:happy_shop/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:user_repository/user_repository.dart';
+
+class MyApp extends StatelessWidget {
+  final UserRepository userRepository;
+  const MyApp(this.userRepository, {super.key});
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider<AuthenticationBloc>(
+      create: (context) => AuthenticationBloc(userRepository: userRepository),
+      child: MyAppView(),
+    );
+  }
+}
--- happy_shop/test/widget_test.dart
+++ happy_shop/test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:happy_shop/app_view.dart';
 
 import 'package:happy_shop/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyAppView());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
