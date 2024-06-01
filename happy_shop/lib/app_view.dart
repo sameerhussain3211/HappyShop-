7551dc16b983b01a59b9af269a5098eeeee3d72f
sameerhussain3211 <sameerhussain44332211@gmail.com>
2024-06-02 02:01:36 +0500

    forth

--- happy_shop/lib/app_view.dart
+++ happy_shop/lib/app_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:happy_shop/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:happy_shop/screens/auth/views/welcome_screen.dart';
+import 'package:happy_shop/screens/home/views/homeScreen.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
@@ -7,6 +11,22 @@ class MyAppView extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "happy Shop",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: ColorScheme.light(
+              background: Colors.green.shade100,
+              onBackground: Colors.black,
+              primary: Colors.blue,
+              onPrimary: Colors.white)),
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state.status == AuthenticationStatus.authenticated) {
+            return Homescreen();
+          } else {
+            return WelcomeScreen();
+          }
+        },
+      ),
     );
   }
 }
